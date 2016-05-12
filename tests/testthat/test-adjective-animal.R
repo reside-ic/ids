75453d7bcbe0e1f2599c3f7f7ced9a75842f2c89
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-05-12 14:23:43 +0100

    Tests for adjective_animal

---
+++ tests/testthat/test-adjective-animal.R
@@ -0,0 +1,25 @@
+context("adjective animal")
+
+test_that("basic", {
+  res <- adjective_animal()
+  expect_is(res, "character")
+  expect_equal(length(res), 1)
+  expect_match(res, "^[a-z]+_[a-z]+$")
+})
+
+test_that("length", {
+  expect_equal(length(adjective_animal(5)), 5)
+  expect_equal(adjective_animal(0), character(0))
+})
+
+test_that("style", {
+  expect_match(adjective_animal(style="Pascal"), "^[A-Z][a-z]+[A-Z][a-z]+$")
+  expect_match(adjective_animal(style="camel"), "^[a-z]+[A-Z][a-z]+$")
+  expect_match(adjective_animal(style="snake"), "^[a-z]+_[a-z]+$")
+  expect_match(adjective_animal(style="kebab"), "^[a-z]+-[a-z]+$")
+  expect_match(adjective_animal(style="lower"), "^[a-z]+ [a-z]+$")
+  expect_match(adjective_animal(style="upper"), "^[A-Z]+ [A-Z]+$")
+  expect_match(adjective_animal(style="constant"), "^[A-Z]+_[A-Z]+$")
+  expect_match(adjective_animal(style="title"), "^[A-Z][a-z]+ [A-Z][a-z]+$")
+  expect_match(adjective_animal(style="sentence"), "^[A-Z][a-z]+ [a-z]+$")
+})
