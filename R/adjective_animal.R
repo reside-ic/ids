ec7740dd515bec15aba8be6bba9f2c64a8fb11cc
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-06-01 08:11:34 +0100

    Separate lengths for adjective and animal

--- R/adjective_animal.R
+++ R/adjective_animal.R
@@ -10,20 +10,32 @@
 ##' @param max_len The maximum length of a word part to include (this
 ##'   may be useful because some of the names are rather long.  This
 ##'   stops you generating a
-##'   \code{hexakosioihexekontahexaphobic_queenalexandrasbirdwingbutterfly})
+##'   \code{hexakosioihexekontahexaphobic_queenalexandrasbirdwingbutterfly}).
+##'   A vector of length 2 ca be passed in here in which case the
+##'   first element will apply to the adjectives (all of them) and the
+##'   second element will apply to the animals.
+##'
 ##' @inheritParams ids
 ##' @export
 ##' @examples
 ##' adjective_animal()
 ##' adjective_animal(n_adjectives=3)
 ##' adjective_animal(style="lower")
+##' adjective_animal(10, max_len=6)
+##' adjective_animal(10, max_len=c(6, Inf))
 adjective_animal <- function(n=1, n_adjectives=1, style="snake", max_len=Inf) {
-  if (is.finite(max_len)) {
-    if (max_len < 3) {
+  if (any(is.finite(max_len))) {
+    if (any(max_len < 3)) {
       stop("max_len must be at least 3")
     }
-    gfycat_adjectives <- gfycat_adjectives[nchar(gfycat_adjectives) <= max_len]
-    gfycat_animals <- gfycat_animals[nchar(gfycat_animals) <= max_len]
+    if (length(max_len) == 1L) {
+      max_len <- rep_len(max_len, 2)
+    } else if (length(max_len) != 2L) {
+      stop("max_len must be length 1 or 2")
+    }
+    gfycat_adjectives <-
+      gfycat_adjectives[nchar(gfycat_adjectives) <= max_len[1]]
+    gfycat_animals <- gfycat_animals[nchar(gfycat_animals) <= max_len[2]]
   }
   vals <- c(rep(list(gfycat_adjectives), n_adjectives), list(gfycat_animals))
   ids(n, vals=vals, style=style)
