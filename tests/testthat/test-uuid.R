8564bf430cac0b9aa75c321e83180a8c4d3e6f72
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-09-08 18:13:06 +0100

    Update and expand uuid testing

--- tests/testthat/test-uuid.R
+++ tests/testthat/test-uuid.R
@@ -1,13 +1,31 @@
 context("uuid")
 
+RE_UUID <- "^[[:xdigit:]]{8}-([[:xdigit:]]{4}-){3}[[:xdigit:]]{12}$"
+
 test_that("uuid", {
-  expect_that(uuid(0), equals(character(0)))
+  expect_equal(uuid(0), character(0))
 
   x1 <- uuid(1)
-  expect_that(length(x1), equals(1))
-  expect_that(x1, is_a("character"))
+  expect_equal(length(x1), 1)
+  expect_is(x1, "character")
+  expect_match(x1, RE_UUID)
 
   x10 <- uuid(10)
-  expect_that(length(x10), equals(10))
-  expect_that(x10, is_a("character"))
+  expect_equal(length(x10), 10)
+  expect_is(x10, "character")
+  expect_true(all(grepl(RE_UUID, x10)))
+
+  expect_true(all(grepl("^[[:xdigit:]]{32}$", uuid(10, drop_hyphens = TRUE))))
+
+  expect_match(uuid(use_time = TRUE), RE_UUID)
+})
+
+test_that("bind args", {
+  f <- uuid(NULL)
+  expect_is(f, "function")
+  expect_equal(as.list(formals(f)), list(n = 1))
+  expect_match(f(), RE_UUID)
+
+  g <- uuid(NULL, TRUE)
+  expect_match(g(), "^[[:xdigit:]]{32}$")
 })
