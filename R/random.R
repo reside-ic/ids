5140076316ecb01d8f4d4099a1b5afae0de9de4d
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-09-08 19:05:17 +0100

    Examples and re-roxygenisation

--- R/random.R
+++ R/random.R
@@ -19,6 +19,36 @@
 #'
 #' @export
 #' @author Rich FitzJohn
+#' @examples
+#' # Generate a random id:
+#' random_id()
+#'
+#' # Generate 10 of them!
+#' random_id(10)
+#'
+#' # Different length ids
+#' random_id(bytes = 8)
+#' # (note that the number of characters is twice the number of bytes)
+#'
+#' # The ids are not affected by R's RNG state:
+#' set.seed(1)
+#' (id1 <- random_id())
+#' set.seed(1)
+#' (id2 <- random_id())
+#' # The generated identifiers are different, despite the seed being the same:
+#' id1 == id2
+#'
+#' # If you need these identifiers to be reproducible, pass use_openssl = FALSE
+#' set.seed(1)
+#' (id1 <- random_id(use_openssl = FALSE))
+#' set.seed(1)
+#' (id2 <- random_id(use_openssl = FALSE))
+#' # This time they are the same:
+#' id1 == id2
+#'
+#' # Pass \code{n = NULL} to generate a function that binds your arguments:
+#' id8 <- random_id(NULL, bytes = 8)
+#' id8(10)
 random_id <- function(n = 1, bytes = 16, use_openssl = TRUE) {
   if (is.null(n)) {
     force(bytes)
